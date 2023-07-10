67c0719412f89c9a925c49caced431cd5e1413d5
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-07-10 18:38:31 +0800

    Moved session and signIn inside NextAuth callbacks

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,34 +10,37 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-        session.user.id = sessionUser._id.toString()
-        return session
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB()
-            // check if user already exist
-            const userExist = await User.findOne({
-                email: profile.email
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
             })
-            // else create new user
-            if (!userExist) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(' ', '').toLowerCase(),
-                    image: profile.picture
+            session.user.id = sessionUser._id.toString()
+            return session
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB()
+                // check if user already exist
+                const userExist = await User.findOne({
+                    email: profile.email
                 })
+                // else create new user
+                if (!userExist) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(' ', '').toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true
+            } catch (error) {
+                console.error(`Error at signIn Route: ${error}`)
+                return false
             }
-            return true
-        } catch (error) {
-            console.error(`Error at signIn Route: ${error}`)
-            return false
         }
-    }
+    },
+
 })
 
 export { handler as GET, handler as POST }
